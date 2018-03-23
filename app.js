13356b0e08daa0b6bbe8dcc8fa67c681468730db
student <student@A104.MISZCZOWIE>
2018-03-23 14:41:39 +0100

    test

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send the response body as "Hello World"
-    response.end('Hello World Swarm!\n');
+    response.end('Hello World Swarm! with latest\n');
 }).listen(80);
